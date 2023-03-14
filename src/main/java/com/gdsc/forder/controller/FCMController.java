481771a6c128e214054331df8d36bb1a972dd3e0
ohyujeong <ohlife4@gmail.com>
2023-03-14 15:01:43 +0900

    [Feat] 푸시 알림 API

---
+++ src/main/java/com/gdsc/forder/controller/FCMController.java
@@ -0,0 +1,44 @@
+package com.gdsc.forder.controller;
+
+import com.gdsc.forder.dto.PushNotificationRequest;
+import com.gdsc.forder.dto.PushNotificationResponse;
+import com.gdsc.forder.service.PushNotificationService;
+import io.swagger.annotations.Api;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "푸시 알림 API")
+@RequestMapping("/fcm")
+@RestController
+public class FCMController {
+
+    private PushNotificationService pushNotificationService;
+
+    public FCMController(PushNotificationService pushNotificationService) {
+        this.pushNotificationService = pushNotificationService;
+    }
+
+    @PostMapping("/notification/topic")
+    public ResponseEntity sendNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationWithoutData(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @PostMapping("/notification/token")
+    public ResponseEntity sendTokenNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationToToken(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @PostMapping("/notification/data")
+    public ResponseEntity sendDataNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotification(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+
+}
